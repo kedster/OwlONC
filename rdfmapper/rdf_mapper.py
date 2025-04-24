3a40dcb3fb7bcbf105fcbdbe8a2af816dd4c6bc1
Seth Keddy <skeddy@nwti.edu>
2025-04-24 13:54:01 -0500

    Update rdf_mapper.py

---
+++ rdfmapper/rdf_mapper.py
@@ -0,0 +1,13 @@
+```python
+from rdflib import Graph, URIRef, Literal, Namespace
+
+def generate_rdf(data, namespace_uri="http://example.org/"):
+    ns = Namespace(namespace_uri)
+    g = Graph()
+    
+    for idx, item in enumerate(data):
+        subject = URIRef(f"{ns}item{idx}")
+        for key, value in item.items():
+            g.add((subject, ns[key], Literal(value)))
+
+    return g.serialize(format="turtle")
